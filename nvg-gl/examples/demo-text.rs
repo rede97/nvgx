681e070fea5c1e793dd42350236d88ea3d606a7a
Kanne <Kanne@noreply.gitcode.com>
2025-05-13 17:42:39 +0800

    update demo

--- nvg-gl/examples/demo-text.rs
+++ nvg-gl/examples/demo-text.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use nvg::*;
 
 mod demo;
@@ -12,41 +14,49 @@ impl<R: RendererDevice> demo::Demo<R> for DemoText {
         ctx.stroke_paint((1.0, 0.0, 0.0));
         ctx.stroke()?;
 
-        ctx.font_size(16.0);
-        ctx.fill_paint((1.0, 1.0, 0.0));
+        ctx.save();
+        {
+            ctx.font_size(16.0);
+            ctx.fill_paint((1.0, 1.0, 0.0));
 
-        // horz align
-        ctx.text_align(nvg::Align::LEFT);
-        ctx.text((150, 60), "left")?;
+            // horz align
+            ctx.text_align(nvg::Align::LEFT);
+            ctx.text((150, 60), "left")?;
 
-        ctx.text_align(nvg::Align::CENTER);
-        ctx.text((150, 80), "center")?;
+            ctx.text_align(nvg::Align::CENTER);
+            ctx.text((150, 80), "center")?;
 
-        ctx.text_align(nvg::Align::RIGHT);
-        ctx.text((150, 100), "right")?;
+            ctx.text_align(nvg::Align::RIGHT);
+            ctx.text((150, 100), "right")?;
 
-        // vert align
-        ctx.begin_path();
-        ctx.move_to((5, 270));
-        ctx.line_to((300, 270));
-        ctx.stroke_paint((1.0, 0.0, 0.0));
-        ctx.stroke()?;
+            // vert align
+            ctx.begin_path();
+            ctx.move_to((5, 270));
+            ctx.line_to((300, 270));
+            ctx.stroke_paint((1.0, 0.0, 0.0));
+            ctx.stroke()?;
 
-        ctx.text_align(nvg::Align::TOP);
-        ctx.text((5, 270), "top")?;
+            ctx.text_align(nvg::Align::TOP);
+            ctx.text((5, 270), "top")?;
 
-        ctx.text_align(nvg::Align::MIDDLE);
-        ctx.text((50, 270), "middle")?;
+            ctx.text_align(nvg::Align::MIDDLE);
+            ctx.text((50, 270), "middle")?;
 
-        ctx.text_align(nvg::Align::BOTTOM);
-        ctx.text((120, 270), "bottom")?;
+            ctx.text_align(nvg::Align::BOTTOM);
+            ctx.text((120, 270), "bottom")?;
 
-        ctx.text_align(nvg::Align::BASELINE);
-        ctx.text((200, 270), "baseline")?;
+            ctx.text_align(nvg::Align::BASELINE);
+            ctx.text((200, 270), "baseline")?;
 
-        // spaces
-        ctx.text((200, 300), "a b  c   d")?;
+            // spaces
+            ctx.text((200, 300), "a b  c   d")?;
+        }
+        ctx.restore();
 
+        ctx.font_size(60.0);
+        ctx.rotate(PI / 4.0);
+        ctx.fill_paint(Color::rgb_i(0x00, 0xC8, 0xFF));
+        ctx.text((_width / 4.0, 0.0), "Hello World")?;
         Ok(())
     }
 }
